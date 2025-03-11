1746e12ddeaa1d2a1d99991d18c9b10a522bae45
IDGS-801-22002360 <81442@alumnos.utleon.edu.mx>
2025-03-10 18:06:23 -0600

    Reorganizacion del proyecto

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ csrf = CSRFProtect(app)
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index")
 def index():
-    return render_template("mainClientes.html")
+    return render_template("client/mainClientes.html")
 
 #!=============== Modulo de Productos ===============#  
 
@@ -21,7 +21,7 @@ def index():
 def galletas():
     form = LoteForm()
     productos = db.session.execute(text("SELECT * FROM productosTerminados WHERE estatus=1")).fetchall()
-    return render_template("galletas.html", productos=productos, form=form)
+    return render_template("admin/galletas.html", productos=productos, form=form)
 
 @app.route("/guardarLote", methods=["POST"])
 def guardarLote():
@@ -33,15 +33,16 @@ def guardarLote():
         return jsonify({'success': True, 'message': 'Lote guardado Correctamente'})
     return jsonify({'success': False, 'message': 'Error al guardar'})
 
-#   @app.route("/guardarPaquete")
+#   @app.route("/eliminarPaquete")
 #       def guardarPaquete():
-#           
+#           if form.validate
 #
-#
+
+#!=============== Modulo de Insumos ===============#
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('admin/404.html'), 404
 
 if __name__ == '__main__':
     csrf.init_app(app)
